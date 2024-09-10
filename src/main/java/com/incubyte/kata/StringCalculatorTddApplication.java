9055a4ffc5315305533c187d1132ffeec22e339a
mukulpandit <mukul2299pandit@gmail.com>
2024-09-10 18:39:27 +0530

    "Project initialized"

---
+++ src/main/java/com/incubyte/kata/StringCalculatorTddApplication.java
@@ -0,0 +1,13 @@
+package com.incubyte.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalculatorTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalculatorTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incubyte/kata/StringCalculatorTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubyte.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
